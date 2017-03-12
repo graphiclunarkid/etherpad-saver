a7586172377d59fc06bbe4be69335f2cc9bfe3b2
Richard King <richard@richardskingdom.net>
2017-03-12 23:37:08 +0100

    Refactor & tidy-up
    - BUGFIX: Update wget options to remove warning message
    - REFACTOR: Remove duplicate 'multi' script
    - REFACTOR: Improve readability by renaming some files, variables
    - REFACTOR: Improve git initialisation
    - REFACTOR: Only commit changes to repository if wget succeeds
    - MAINT: Improve code comments throughout
    - MAINT: Update README.md
    
    Closes #1

--- README.md
+++ README.md
@@ -3,29 +3,30 @@ etherpad-saver
 Many public etherpad instances delete pads after a certain time period.
 Instances can become flaky, delete pads at random or even vanish completely.
 Sometimes malicious users delete everything you typed and the timeline feature
-screws up so you can't get it back. So I wrote this script to grab copies of an
-etherpad page (or similar) and keep track of changes using git. I run it from
-cron when I'm working on an important pad to make sure I don't lose anything.
+screws up so you can't get it back.
 
-Pads are saved on filenames related to their URL. For this purpose, URL "/" have
-been replaced by "." to for valid *nix filenames.
+This script grabs copies of one or more etherpad pages and keeps track of
+changes using git. Run it from cron when you're working on an important pad to
+make sure you don't lose anything.
+
+Pads are saved in files named according to their URL.
 
 Usage
 -----
 0. Install git and wget if they're not already available
-1. Edit list_pad.example.conf and save as list_pad.conf.
-2. Edit etherpad_saver.example.conf and save as etherpad_saver.conf.
-4. Run etherpad-saver-multi.sh
+1. Edit pad_list.example.conf and save as pad_list.conf
+2. Edit etherpad_saver.example.conf and save as etherpad_saver.conf
+3. Run etherpad-saver.sh
 
 Scheduling snapshots
 --------------------
 Run crontab -e and append the following line (or similar):
 
-*/10 * * * * /home/user/path/to/etherpad-saver-multi.sh > /dev/null 2>&1
+`*/10 * * * * /home/user/path/to/etherpad-saver.sh > /dev/null 2>&1`
 
-This will run the script every 10 minutes. Nothing happens if the pad hasn't
-changed. If it has changed the new version will be downloaded and committed to
-the git repository.
+This will run the script every 10 minutes. Nothing happens if no pads have
+changed. New versions of changed pads will be downloaded and committed to the
+git repository individually.
 
 Safety
 ------
--- etherpad-saver.sh
+++ etherpad-saver.sh
@@ -1,29 +1,77 @@
 #!/bin/bash
+#
+# Automatically download a list of etherpads where their URLs are containted in
+# the file pad_list.conf (one per line)
 
-URL=https://pad.riseup.net/p
-PAD=B7VT41OAfOe3
-FORMAT=txt
-WGET_OPTS="-N --no-check-certificate -nd"
-LOCALDIR=(~/etherpad-saver/)
+# Source the configuration file
+. etherpad_saver.conf
 
+WGET_OPTS="--no-check-certificate -nd"
+WGET=`which wget`
+
+# Create LOCALDIR if it doesn't exist already
 if [ ! -d $LOCALDIR ] ; then
-	mkdir -p $LOCALDIR
+        mkdir -p $LOCALDIR
 fi
 
-wget $WGET_OPTS -P $LOCALDIR $URL/$PAD/export/$FORMAT
-
+# Initialize git in LOCALDIR if necessary.
+# Do this in a sub-shell to avoid changing the user's current working
+# directory.
 (
-	cd $LOCALDIR
+        cd $LOCALDIR
 
-	if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
-		git init -q
-	fi
-
-	git update-index -q --refresh
-
-	if ! git diff-index --quiet HEAD -- > /dev/null 2>&1 ; then
-		git add .
-		git commit -a -m"Change detected - automatic commit"
-	fi
+        if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
+                git init -q
+        fi
 )
+
+# Download each pad. URLs of the pads are contained in the file pad_list.conf
+while read line
+do
+        # Remove leading whitespace characters from the line
+        line="${line#"${line%%[![:space:]]*}"}"
+
+        # Skip the line if it is empty.
+        [ -z "$line" ] && continue
+
+        # Skip the line if it starts with '#' - this is a comment.
+        [[ $line = \#* ]] && continue
+
+        # The URL is the portion of the line up to, but not including, the last
+        # "/" character.
+        URL=$(echo $line | sed s,/[^/]*$,,)
+
+        # Replace the "/" characters in the url with "." characters for a *nix
+        # compatible filename.
+        FILENAME=$(echo $line | tr '/' '.')
+
+        # Remove "https:.." from the start of the *nix compatible filename.
+        FILENAME="${FILENAME##*:..}"
+
+        # The PAD is the portion of the line after the last "/" character.
+        PAD=$(echo $line | sed s,^.*/,,)
+
+        # Some etherpad services format their APIs differently.
+        if [[ $URL =~ .*etherpad.* ]]; then
+                wget $WGET_OPTS -P $LOCALDIR $URL/ep/pad/export/$PAD/latest?format=$FORMAT -O $FILENAME.$FORMAT.temp
+        else  # Other pads than might use other url schema (like the framapad)
+                wget $WGET_OPTS -P $LOCALDIR $URL/$PAD/export/$FORMAT -O $FILENAME.$FORMAT.temp
+        fi
+
+        # If wget succeeded (exit code == 0).
+        if [ "$?" = "0" ]; then
+                # Move the temp file to LOCALDIR and commit it to the git repository.
+                mv $FILENAME.$FORMAT.temp $LOCALDIR$FILENAME.$FORMAT
+                (
+                        cd $LOCALDIR
+                        git update-index -q --refresh
+                        git add .
+                        git commit -a -m"$PAD Change detected - automatic commit"
+                )
+        else
+                # Wget failed. Delete the temporary file.
+                rm $FILENAME.$FORMAT.temp
+        fi
+
+done < pad_list.conf
 
